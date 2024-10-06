d0594b45be02f1d6e913df90c266d74773497d8b
Chukwuka Uba <promiseuba67@gmail.com>
2024-10-06 12:54:52 +0100

    added kols list caching

--- src/app/play/attributes-game/page.tsx
+++ src/app/play/attributes-game/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AttributesGamePageClient from "./attributesGameClient";
 import { fetchKOLs } from "@/lib/api";
+import { appConfig } from "@/lib/config";
 
 async function AttributesGamePage() {
-  const kols = await fetchKOLs();
-  console.log(kols);
-  return <AttributesGamePageClient kols={kols} />;
+  const response = await fetch(`${appConfig.apiBaseUrl}/api/v1/kols`);
+  const data = await response.json();
+  console.log("data:", data);
+
+  return <AttributesGamePageClient kols={data} />;
 }
 
 export default AttributesGamePage;
