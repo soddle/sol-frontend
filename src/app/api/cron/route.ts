2f95c993888980cc9d1f1bc8c7a42be20a1aa0fc
Chukwuka Uba <promiseuba67@gmail.com>
2024-10-20 19:26:23 +0100

    update cron route

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -17,59 +17,60 @@ import { SoddleGame } from "@/lib/constants/soddle_game";
 // export const runtime = "nodejs";
 
 export async function GET(request: Request) {
-  try {
-    // Verify authentication token
-    const authHeader = request.headers.get("authorization");
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-      logger.warn("Unauthorized access attempt to cron route");
+  return NextResponse.json({ success: true, message: "Hello Cron!!" });
+  // try {
+  //   // Verify authentication token
+  //   const authHeader = request.headers.get("authorization");
+  //   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //     logger.warn("Unauthorized access attempt to cron route");
 
-      return Response.json({ error: "Unauthorized" }, { status: 401 });
-    }
+  //     return Response.json({ error: "Unauthorized" }, { status: 401 });
+  //   }
 
-    // Apply rate limiting
-    const { success } = await rateLimit(request);
-    if (!success) {
-      logger.warn("Rate limit exceeded for cron route");
-      return NextResponse.json(
-        { error: "Rate limit exceeded" },
-        { status: 429 }
-      );
-    }
+  //   // Apply rate limiting
+  //   const { success } = await rateLimit(request);
+  //   if (!success) {
+  //     logger.warn("Rate limit exceeded for cron route");
+  //     return NextResponse.json(
+  //       { error: "Rate limit exceeded" },
+  //       { status: 429 }
+  //     );
+  //   }
 
-    const connection = new Connection(
-      chainConfigs.SOLANA.networks[
-        chainConfigs.SOLANA.defaultNetwork
-      ]?.rpcEndpoint!,
-      {
-        commitment: "confirmed",
-      }
-    );
+  //   const connection = new Connection(
+  //     chainConfigs.SOLANA.networks[
+  //       chainConfigs.SOLANA.defaultNetwork
+  //     ]?.rpcEndpoint!,
+  //     {
+  //       commitment: "confirmed",
+  //     }
+  //   );
 
-    // Use a more secure method to retrieve the keypair
-    const cronKeypair = await getSecureKeypair();
-    const programId = new PublicKey(chainConfigs.SOLANA.contractAddresses.game);
+  //   // Use a more secure method to retrieve the keypair
+  //   const cronKeypair = await getSecureKeypair();
+  //   const programId = new PublicKey(chainConfigs.SOLANA.contractAddresses.game);
 
-    const wallet = new anchor.Wallet(cronKeypair);
+  //   const wallet = new anchor.Wallet(cronKeypair);
 
-    const anchorProvider = new anchor.AnchorProvider(connection, wallet);
+  //   const anchorProvider = new anchor.AnchorProvider(connection, wallet);
 
-    anchor.setProvider(anchorProvider);
+  //   anchor.setProvider(anchorProvider);
 
-    const [gameStatePDA] = PublicKey.findProgramAddressSync(
-      [Buffer.from("game_state")],
-      programId
-    );
-    const program = anchor.workspace.Game as anchor.Program<SoddleGame>;
-    const gameAccount = program.account.gameState.fetch(gameStatePDA);
+  //   const [gameStatePDA] = PublicKey.findProgramAddressSync(
+  //     [Buffer.from("game_state")],
+  //     programId
+  //   );
+  //   const program = anchor.workspace.Game as anchor.Program<SoddleGame>;
+  //   const gameAccount = program.account.gameState.fetch(gameStatePDA);
 
-    console.log(gameAccount);
+  //   console.log(gameAccount);
 
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    logger.error("Failed to initialize game:", error);
-    return NextResponse.json(
-      { success: false, error: "Failed to initialize game" },
-      { status: 500 }
-    );
-  }
+  //   return NextResponse.json({ success: true });
+  // } catch (error) {
+  //   logger.error("Failed to initialize game:", error);
+  //   return NextResponse.json(
+  //     { success: false, error: "Failed to initialize game" },
+  //     { status: 500 }
+  //   );
+  // }
 }
